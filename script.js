764e3bf5796f91d51bf698315f9cdfe553b4b9dd
altbdoor <lancersupraskyline@gmail.com>
2018-12-06 22:49:03 +0800

    throw ffz into the mix

--- script.js
+++ script.js
@@ -9,39 +9,65 @@
     ])
 
     .controller('EmoteDisplayController', [
-        '$http',
-        function($http) {
+        '$http', '$q',
+        function($http, $q) {
             const vm = this
 
-            vm.channelNotFound = false
             vm.emotes = []
             vm.searchCompleted = false
 
             vm.searchChannel = searchChannel
-            vm.getImageUrl = getImageUrl
 
             // =====
 
             function searchChannel(form) {
-                vm.channelNotFound = false
                 vm.emotes = []
                 vm.searchCompleted = false
 
                 if (form.$valid) {
-                    $http.get(`https://api.betterttv.net/2/channels/${form.channelName.$modelValue}`).then((res) => {
-                        vm.emotes = res.data.emotes
-                        vm.urlTemplate = res.data.urlTemplate
-                    }).catch(() => {
-                        vm.channelNotFound = true
+                    let bttvPromise = $q.resolve()
+                    let ffzPromise = $q.resolve()
+
+                    if (['all', 'bttv'].indexOf(form.emoteType.$modelValue) !== -1) {
+                        bttvPromise = $http.get(`https://api.betterttv.net/2/channels/${form.channelName.$modelValue}`).then((res) => {
+                            return res.data.emotes.map((emote) => ({
+                                type: 'bttv',
+                                code: emote.code,
+                                img: getBttvImageUrl(res.data.urlTemplate, emote.id),
+                            }))
+                        }).catch(() => {
+                            return null
+                        })
+                    }
+
+                    if (['all', 'ffz'].indexOf(form.emoteType.$modelValue) !== -1) {
+                        ffzPromise = $http.get(`https://api.frankerfacez.com/v1/room/${form.channelName.$modelValue}`).then((res) => {
+                            const roomSet = res.data.room.set
+                            return res.data.sets[roomSet].emoticons.map((emote) => {
+                                const urlKey = Object.keys(emote.urls).sort().pop()
+
+                                return {
+                                    type: 'ffz',
+                                    code: emote.name,
+                                    img: emote.urls[urlKey],
+                                }
+                            })
+                        }).catch(() => {
+                            return null
+                        })
+                    }
+
+                    $q.all([bttvPromise, ffzPromise]).then((promiseList) => {
+                        vm.emotes = promiseList.filter((data) => data)
+                            .reduce((acc, list) => acc.concat(list), [])
                     }).finally(() => {
                         vm.searchCompleted = true
                     })
                 }
             }
 
-            function getImageUrl(urlTemplate, imageId) {
-                const url = urlTemplate.replace('{{image}}', '3x').replace('{{id}}', imageId)
-                return url
+            function getBttvImageUrl(urlTemplate, imageId) {
+                return urlTemplate.replace('{{image}}', '3x').replace('{{id}}', imageId)
             }
         }
     ])
